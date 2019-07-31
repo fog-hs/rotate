cfc93ca6e69294d5853bf9ef75d9619e2a52cad1
fog-hs <53497847+fog-hs@users.noreply.github.com>
2019-07-31 03:05:59 +0100

    Add files via upload
    error. builds as lib

---
+++ src - Copy/example/LinearTest.hs
@@ -0,0 +1,9 @@
+module LinearTest where
+
+import Linear.TestRotate
+
+main :: IO ()
+main = (putStrLn . show) testRotate 
+
+
+
---
+++ src - Copy/example/VectTest.hs
@@ -0,0 +1,6 @@
+module VectTest where
+
+import Vect.TestRotate
+
+main :: IO ()
+main = (putStrLn . show) testRotate 
